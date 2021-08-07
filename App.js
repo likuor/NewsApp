e222579947d507ac6318efcc44b83c458a071d96
Koki Sakai <likuor0528@gmail.com>
2021-08-07 14:03:23 +0900

    0807_create list of news made by axios with NewsApi

--- App.js
+++ App.js
@@ -1,21 +1,46 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState, useEffect} from 'react';
+import {StyleSheet, FlatList, SafeAreaView} from 'react-native';
+import ListItem from './components/ListItem';
+import Constants from 'expo-constants';
+import axios from 'axios';
+
+const URL = `https://newsapi.org/v2/top-headlines?country=jp&apiKey=${Constants.manifest.extra.newsApiKey}`;
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    const [articles, setArticles] = useState([]);
+    useEffect(() => {
+        fetchArticles();
+    }, []);
+
+    const fetchArticles = async () => {
+        try {
+            const response = await axios.get(URL);
+            setArticles(response.data.articles);
+        } catch (error) {
+            console.error(error);
+        }
+    };
+
+    return (
+        <SafeAreaView style={styles.container}>
+            <FlatList
+                data={articles}
+                renderItem={({item}) => (
+                    <ListItem
+                        imageUrl={item.urlToImage}
+                        title={item.title}
+                        author={item.author}
+                    />
+                )}
+                keyExtractor={(item, index) => index.toString()}
+            />
+        </SafeAreaView>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+    },
 });
